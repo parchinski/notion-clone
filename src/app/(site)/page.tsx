8989f07b844816287e3e230f87842b6bed32d09e
parchinski <bryantpdev@proton.me>
2024-06-18 03:00:38 -0400

    feat: :sparkles: Added CLIENTS section
    Clients section completed with individual png logos for each of the clients and cool gradient effects with tailwindcss.

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import TitleSection from "@/components/landing-page/title-section";
 import Banner from "../../../public/appBanner.png";
+import { CLIENTS } from "@/lib/constants";
 
 const HomePage = () => {
   return (
-    <section>
-      <div className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
+    <>
+      <section className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
         <TitleSection
           pill="✨ Your workspace, Perfected"
           title="All-In-One Collaboration and Productivity Platform"
@@ -24,8 +25,29 @@ const HomePage = () => {
           <Image src={Banner} alt="Application Banner" />
           <div className="bottom-0 top-[-50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10" />
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="relative">
+        <div className="overflow-hidden flex absolute after:content[''] after:dark:from-brand-dark after:to-transparent after:from-background after:bg-gradient-to-l after:right-0 after:bottom-0 after:w-20 after:z-10 after:absolute before:content[''] before:dark:from-brand-dark before:to-transparent before:from-background before:bg-gradient-to-r before:left-0 before:bottom-0 before:w-20 before:z-10 before:absolute">
+          {[...Array(2)].map((arr) => (
+            <div key={arr} className="flex flex-nowrap animate-slide">
+              {CLIENTS.map((client) => (
+                <div
+                  key={client.alt}
+                  className="relative w-[200px] m-20 shrink-0 flex items-center"
+                >
+                  <Image
+                    src={client.logo}
+                    alt={client.alt}
+                    width={200}
+                    className="object-contain max-w-none"
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
