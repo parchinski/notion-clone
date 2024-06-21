0c41b7d9daf44b04be6fd09564677247f27b0b90
parchinski <bryantpdev@proton.me>
2024-06-21 16:46:12 -0400

    style: :art: made tailwind classes more readable

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
+import { CLIENTS } from "@/lib/constants";
 import TitleSection from "@/components/landing-page/title-section";
 import Banner from "../../../public/appBanner.png";
-import { CLIENTS } from "@/lib/constants";
 
 const HomePage = () => {
   return (
@@ -35,30 +35,91 @@ const HomePage = () => {
         >
           <Button
             variant="secondary"
-            className="w-full rounded-[10px] p-6 text-2xl bg-background"
+            className="w-full
+            rounded-[10px]
+            p-6
+            text-2xl
+            bg-background"
           >
             Get Cypress Free
           </Button>
         </div>
-        <div className="md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]">
+        <div
+          className="md:mt-[-90px]
+        sm:w-full
+        w-[750px]
+        flex
+        justify-center
+        items-center
+        mt-[-40px]
+        relative
+        sm:ml-0
+        ml-[-50px]"
+        >
           <Image src={Banner} alt="Application Banner" />
-          <div className="bottom-0 top-[-50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10" />
+          <div
+            className="bottom-0
+          top-[-50%]
+          bg-gradient-to-t
+          dark:from-background
+          left-0
+          right-0
+          absolute
+          z-10"
+          />
         </div>
       </section>
       <section className="relative">
-        <div className="overflow-hidden flex absolute after:content[''] after:dark:from-brand-dark after:to-transparent after:from-background after:bg-gradient-to-l after:right-0 after:bottom-0 after:w-20 after:z-10 after:absolute before:content[''] before:dark:from-brand-dark before:to-transparent before:from-background before:bg-gradient-to-r before:left-0 before:bottom-0 before:w-20 before:z-10 before:absolute">
+        <div
+          className="overflow-hidden
+          flex
+          after:content['']
+          after:dark:from-brand-dark
+          after:to-transparent
+          after:from-background
+          after:bg-gradient-to-l
+          after:right-0
+          after:bottom-0
+          after:top-0
+          after:w-20
+          after:z-10
+          after:absolute
+
+          before:content['']
+          before:dark:from-brand-dark
+          before:to-transparent
+          before:from-background
+          before:bg-gradient-to-r
+          before:left-0
+          before:top-0
+          before:bottom-0
+          before:w-20
+          before:z-10
+          before:absolute"
+        >
           {[...Array(2)].map((arr) => (
-            <div key={arr} className="flex flex-nowrap animate-slide">
+            <div
+              key={arr}
+              className="flex
+            flex-nowrap
+            animate-slide"
+            >
               {CLIENTS.map((client) => (
                 <div
                   key={client.alt}
-                  className="relative w-[200px] m-20 shrink-0 flex items-center"
+                  className="relative
+                  w-[200px]
+                  m-20
+                  shrink-0
+                  flex
+                  items-center"
                 >
                   <Image
                     src={client.logo}
                     alt={client.alt}
                     width={200}
-                    className="object-contain max-w-none"
+                    className="object-contain
+                    max-w-none"
                   />
                 </div>
               ))}
@@ -66,15 +127,32 @@ const HomePage = () => {
           ))}
         </div>
       </section>
-      <section className="px-4 sm:px-6 flex justify-center items-center flex-col relative">
+      <section
+        className="px-4
+      sm:px-6
+      flex
+      justify-center
+      items-center
+      flex-col
+      relative"
+      >
         {/* purple gradient-glow section */}
-        <div className="w-[30%] blur-[120px] rounded-full h-32 absolute bg-brand-primaryPurple/50 -z-10 top-22" />
+        <div
+          className="w-[30%]
+        blur-[120px]
+        rounded-full
+        h-32
+        absolute
+        bg-brand-primaryPurple/50
+        -z-10
+        top-22"
+        />
         <TitleSection
           title="Keep track of your meetings all in one place"
           subheading="Experience all the benefits of our platform. Select a plan that suits your needs and take your productivity to new heights."
           pill="Features"
         />
-        <div></div>
+        <div className=""></div>
       </section>
     </>
   );
--- src/components/landing-page/title-section.tsx
+++ src/components/landing-page/title-section.tsx
@@ -13,24 +13,60 @@ const TitleSection: React.FC<TitleSectionProps> = ({
 }) => {
   return (
     <React.Fragment>
-      <section className="flex flex-col gap-4 justify-center items-start md:items-center">
+      <section
+        className="flex
+      flex-col
+      gap-4
+      justify-center
+      items-start
+      md:items-center"
+      >
         <article
-          className="rounded-full p-[1px] text-sm dark:bg-gradient-to-r dark:from-brand-primaryBlue
+          className="rounded-full
+          p-[1px]
+          text-sm
+          dark:bg-gradient-to-r
+          dark:from-brand-primaryBlue
           dark:to-brand-primaryPurple"
         >
-          <div className="rounded-full px-3 py-1 dark:bg-black">{pill}</div>
+          <div
+            className="rounded-full
+          px-3
+          py-1
+          dark:bg-black"
+          >
+            {pill}
+          </div>
         </article>
         {subheading ? (
           <>
-            <h2 className="text-left text-3xl sm:text-5xl sm:max-w-[750-px] md:text-center font-semibold ">
+            <h2
+              className="text-left
+            text-3xl
+            sm:text-5xl
+            sm:max-w-[750-px]
+            md:text-center
+            font-semibold "
+            >
               {title}
             </h2>
-            <p className="dark:text-wash-purple-700 sm:max-w-[450px]md:text-center">
+            <p
+              className="dark:text-wash-purple-700
+            sm:max-w-[450px]
+            md:text-center"
+            >
               {subheading}
             </p>
           </>
         ) : (
-          <h1 className="text-left text-4xl sm:text-6xl sm:max-w-[850px] md:text-center font-semibold">
+          <h1
+            className="text-left
+          text-4xl
+          sm:text-6xl
+          sm:max-w-[850px]
+          md:text-center
+          font-semibold"
+          >
             {title}
           </h1>
         )}
